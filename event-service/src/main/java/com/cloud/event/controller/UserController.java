98ce513a39790598312b7c0005a824ab17db9ced
jarivera <jarivera@stratio.com>
2018-02-01 13:59:23 -0500

    Update user

--- event-service/src/main/java/com/cloud/event/controller/UserController.java
+++ event-service/src/main/java/com/cloud/event/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cloud.event.controller;
 
 import com.cloud.event.domain.User;
+import com.cloud.event.dto.EventDTO;
 import com.cloud.event.dto.UserDTO;
 import com.cloud.event.service.UserService;
 import io.vavr.control.Option;
@@ -20,7 +21,6 @@ public class UserController {
     public ResponseEntity findUserByEmailAndPassword(@PathVariable String email, @PathVariable String password){
 
         Option<UserDTO> user = userService.findUserByEmailAndPassword(email, password);
-
         if(user.isDefined()) {
             return new ResponseEntity<>(user.get(), HttpStatus.OK);
         }
@@ -31,5 +31,15 @@ public class UserController {
     public ResponseEntity saveEvent(@RequestBody UserDTO userDTO){
         User user = userService.saveUser(userDTO);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public ResponseEntity findUserByEmailAndPassword(@RequestBody UserDTO userDTO){
+
+        Option<UserDTO> user = userService.findUserByEmailAndPassword(userDTO.getEmail(), userDTO.getPassword());
+        if(user.isDefined()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- event-service/src/main/java/com/cloud/event/dto/UserDTO.java
+++ event-service/src/main/java/com/cloud/event/dto/UserDTO.java
@@ -27,4 +27,20 @@ public class UserDTO implements Serializable {
     @JsonFormat(pattern = Constants.PATTERN_DATE)
     private Date registerDate;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
